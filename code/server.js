6974af9ded0e3fe41d2733e3201056f5a2f924c0
Mayur Harinkhede <mayur.harinkhede@akshayapatra.org>
2026-08-17 12:40:04 +0530

    Add_self_contained_images_for_cloud_deployment

--- code/server.js
+++ code/server.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname)));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/images', express.static(path.join(__dirname, '../images')));
+app.use(express.static(path.join(__dirname, 'images')));
 app.use(express.static(path.join(__dirname, '../images')));
 
 // Helper to initialize Notion Client
